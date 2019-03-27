abbbf9ea5350049f4ebcaf60ea0a14c374572a23
glyph-cat <b75ggnacj@relay.firefox.com>
2019-03-27 21:55:53 +0800

    2.1.0

---
+++ __tests__/byKey.dev.test.js
@@ -0,0 +1,25 @@
+const langutil = require('../lib/langutil.dev')
+const { createKey } = langutil
+const dictionary = [
+    createKey("HELLO_WORLD", {
+        "en": "Hello world",
+        "zh-cn": "哈咯世界"
+    }),
+    createKey("HELLO_NAME_AND_NAME", {
+        "en": "Hello, %p and %p. ",
+        "zh-cn": "%p，%p，你们好。"
+    }),
+    createKey("HELLO_ESCAPED_AND_NAME", {
+        "en": "Hello, %%p and %p. ",
+        "zh-cn": "%%p，%p，你们好。"
+    }),
+    createKey("ONE_HUNDRED", {
+        "en": 100,
+        "zh-cn": 100
+    }),
+]
+
+langutil.init(dictionary, "en")
+
+const baseImplementation = require('./baseImplementation/dev')
+baseImplementation()
---
+++ __tests__/byLang.dev.test.js
@@ -0,0 +1,20 @@
+const langutil = require('../lib/langutil.dev')
+const dictionary = {
+    "en": {
+        "HELLO_WORLD": "Hello world",
+        "HELLO_NAME_AND_NAME": "Hello, %p and %p. ",
+        "HELLO_ESCAPED_AND_NAME": "Hello, %%p and %p. ",
+        "ONE_HUNDRED": 100
+    },
+    "zh-cn": {
+        "HELLO_WORLD": "哈咯世界",
+        "HELLO_NAME_AND_NAME": "%p，%p，你们好。",
+        "HELLO_ESCAPED_AND_NAME": "%%p，%p，你们好。",
+        "ONE_HUNDRED": 100
+    }
+}
+
+langutil.init(dictionary, "en")
+
+const baseImplementation = require('./baseImplementation/dev')
+baseImplementation()
