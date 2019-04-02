dc983951b4fcb6b9d5222e9f22e6b0d40ccc2346
glyph-cat <b75ggnacj@relay.firefox.com>
2019-04-02 19:41:55 +0800

    v2.2.0

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@
  * @author chin98edwin
  * @copyright Copyright (c) 2018 - 2019, chin98edwin
  * @description Localization for JavaScript made simple.
- * @version 2.1.3
+ * @version 2.2.0
 **/
 
 declare namespace langutil {
