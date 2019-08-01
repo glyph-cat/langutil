253d7f0d7bea8195dd027cb58fb56fb4f275f0a5
chin98edwin <edwin_chin98@hotmail.com>
2019-08-01 10:40:51 +0800

    Added zh-cn to template dictionary

--- dictionary/by_key/index.js
+++ dictionary/by_key/index.js
@@ -3,118 +3,147 @@ const { createKey } = require("langutil");
 module.exports = [
     createKey("ACCOUNT", {
         "en": "Account",
-        "ms": "Akaun"
+        "ms": "Akaun",
+        "zh-cn": "账户",
     }),
     createKey("ADDRESS"), {
         "en": "Address",
-        "ms": "Alamat"
+        "ms": "Alamat",
+        "zh-cn": "地址",
     },
     createKey("BACK", {
         "en": "Back",
-        "ms": "Kembali"
+        "ms": "Kembali",
+        "zh-cn": "返回",
     }),
     createKey("C_PASSWORD", {
         "en": "Confirm password",
-        "ms": "Pastikan kata laluan"
+        "ms": "Pastikan kata laluan",
+        "zh-cn": "确认密码",
     }),
     createKey("CANCEL", {
         "en": "Cancel",
-        "ms": "Batalkan"
+        "ms": "Batalkan",
+        "zh-cn": "取消",
     }),
     createKey("DONE", {
         "en": "Done",
-        "ms": "Selesai"
+        "ms": "Selesai",
+        "zh-cn": "完成",
     }),
     createKey("EMAIL", {
         "en": "Email",
-        "ms": "Emel"
+        "ms": "Emel",
+        "zh-cn": "电邮",
     }),
     createKey("FEMALE", {
         "en": "Female",
-        "ms": "Perempuan"
+        "ms": "Perempuan",
+        "zh-cn": "女",
     }),
     createKey("GENDER", {
         "en": "Gender",
-        "ms": "Jantina"
+        "ms": "Jantina",
+        "zh-cn": "性别",
     }),
     createKey("HELP", {
         "en": "Help",
-        "ms": "Pertolongan"
+        "ms": "Pertolongan",
+        "zh-cn": "帮助",
     }),
     createKey("LOGIN", {
         "en": "Login",
-        "ms": "Log masuk"
+        "ms": "Log masuk",
+        "zh-cn": "登入",
     }),
     createKey("LOGOUT", {
         "en": "Logout",
-        "ms": "Log keluar"
+        "ms": "Log keluar",
+        "zh-cn": "登出",
     }),
     createKey("NAME", {
         "en": "Name",
-        "ms": "Nama"
+        "ms": "Nama",
+        "zh-cn": "姓名",
     }),
     createKey("MALE", {
         "en": "Male",
-        "ms": "Lelaki"
+        "ms": "Lelaki",
+        "zh-cn": "男",
     }),
     createKey("NEXT"), {
         "en": "Next",
-        "ms": "Seterusnya"
+        "ms": "Seterusnya",
+        "zh-cn": "下一项",
     },
     createKey("NO", {
         "en": "No",
         "ms": "Tidak",
+        "zh-cn": "否",
     }),
     createKey("NOTIFICATIONS", {
         "en": "Notifications",
         "ms": "Pemberitahuan",
+        "zh-cn": "通知",
     }),
     createKey("OK", {
         "en": "Ok",
         "ms": "Ok",
+        "zh-cn": "好的",
     }),
     createKey("PASSWORD", {
         "en": "Password",
         "ms": "Kata laluan",
+        "zh-cn": "密码",
     }),
     createKey("PHONE", {
         "en": "Phone",
         "ms": "Telefon",
+        "zh-cn": "电话",
     }),
     createKey("PIN", {
         "en": "Pin",
         "ms": "Pin",
+        "zh-cn": "Pin",
     }),
     createKey("PREVIOUS", {
         "en": "Previous",
-        "ms": "",
+        "ms": "Sebelumnya",
+        "zh-cn": "前一项",
     }),
     createKey("REGISTER", {
         "en": "Register",
         "ms": "Mendaftar",
+        "zh-cn": "注册",
     }),
     createKey("SEND", {
         "en": "Send",
         "ms": "Hantar",
+        "zh-cn": "发送",
     }),
     createKey("SETTINGS", {
         "en": "Settings",
         "ms": "Seting",
+        "zh-cn": "设置",
     }),
     createKey("SIGNUP", {
         "en": "Signup",
         "ms": "Pendaftaran",
+        "zh-cn": "注册",
     }),
     createKey("USER", {
         "en": "User",
         "ms": "Pengguna",
+        "zh-cn": "用户",
     }),
     createKey("USERNAME", {
         "en": "Username",
         "ms": "Nama pengguna",
+        "zh-cn": "用户名",
     }),
     createKey("YES", {
         "en": "Yes",
-        "ms": "Ya"
+        "ms": "Ya",
+        "zh-cn": "是",
     })
 ];
