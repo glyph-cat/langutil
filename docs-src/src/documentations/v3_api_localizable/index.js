0c35887396e48cf459780d4fabb019332fe3018c
glyph-cat <b75ggnacj@relay.firefox.com>
2019-11-02 20:49:05 +0800

    Added doc content for Localizable

--- docs-src/src/documentations/v3_api_localizable/index.js
+++ docs-src/src/documentations/v3_api_localizable/index.js
@@ -1,25 +1,79 @@
 import React from 'react'
-// import { localize } from 'langutil'
+import { localize } from 'langutil'
 import { withLang } from 'langutil/react-additions'
-// import { Body, Code } from '../../components/document'
+import { withProps } from '../../modules'
 import CodeTitle from '../../components/code-title'
-// import ParamList from '../../components/param-list'
-// import { withProps } from '../../modules'
+import { Body, Code } from '../../components/document'
+import ParamList from '../../components/param-list'
+import ReturnType from '../../components/return-type'
+import DeprecationWarning from '../../components/deprecation-warning'
+import { VALUES, STRINGS } from '../../constants'
 
 export default withLang(() => {
 
-  // const params = {
-  //   dict: {
-  //     type: 'object',
-  //     desc: localize('API_PARAM_DICT'),
-  //   },
-  // }
+  const params = {
+
+    // dict: {
+    //   type: 'object',
+    //   desc: localize('API_PARAM_DICT'),
+    // },
+
+    keyword: {
+      type: 'string',
+      desc: localize('API_PARAM_KEYWORD_LOCZ'),
+    },
+    paramArray: {
+      type: 'Array',
+      desc: localize('API_PARAM_PARAMARRAY_LOCZ'),
+      optional: true,
+    },
+    casing: {
+      oneOf: ['lowerCase', 'localeLowerCase', 'localeUpperCase', 'sentenceCase', 'titleCase', 'upperCase'],
+      desc: localize('API_PARAM_CASING'),
+      optional: true,
+    },
+    transform: {
+      type: 'Function',
+      desc: localize('API_PARAM_TRANSFORM'),
+      optional: true,
+    },
+    renderAs: {
+      type: 'any',
+      desc: localize({
+        keyword: 'API_PARAM_RENDER_AS_LOCZ',
+        transform: withProps({
+          span: <Code>{'<span />'}</Code>,
+          renderAsVal: <Code>renderAs='value'</Code>
+        })
+      }),
+      optional: true,
+    },
+    allowEmpty: {
+      type: 'boolean',
+      desc: localize('API_PARAM_ALLOW_EMPTY'),
+      optional: true
+    }
+  }
 
   return (
     <>
-
       <CodeTitle name='Localizable' rType='any' isNotFunction />
-
+      <DeprecationWarning
+        // sinceVersion=''
+        removeDate={VALUES.v240DeprecatedRemovalDate}
+        message={localize({
+          keyword: 'DEPRECATED_MSG_LOCALIZABLE',
+          transform: withProps({
+            renderAs: <Code>renderAs</Code>,
+            locz: <Code>{'<Localizable />'}</Code>,
+            loc: <Code>localize()</Code>,
+            withL: <Code>withLang()</Code>
+          })
+        })}
+      />
+      <Body children={localize('API_DESC_LOCALIZABLE')} />
+      <ParamList data={params} />
+      <ReturnType desc={STRINGS.notAvailableDash} type='React.ReactElement' />
     </>
   )
 
