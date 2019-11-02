da81b75129724dff300c74cbcf1caadec50d1534
glyph-cat <b75ggnacj@relay.firefox.com>
2019-11-02 22:50:03 +0800

    Rebuild docs folder with homepage set to langutil

---
+++ docs/precache-manifest.97c6a9869116bb58d18a0f8b9d25ec85.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1a087ef36f597b14db4b1d4dfbd02c",
+    "url": "/langutil/index.html"
+  },
+  {
+    "revision": "562ccf6a8b51fd375e3e",
+    "url": "/langutil/static/css/0.59fee26f.chunk.css"
+  },
+  {
+    "revision": "23b77970e5383e054a44",
+    "url": "/langutil/static/css/4.d8257110.chunk.css"
+  },
+  {
+    "revision": "bd9a5785aef3532a220b",
+    "url": "/langutil/static/css/5.0ebd6c73.chunk.css"
+  },
+  {
+    "revision": "d9301644693032e88be0",
+    "url": "/langutil/static/css/6.87544b52.chunk.css"
+  },
+  {
+    "revision": "cd37728678fa45e5ed03",
+    "url": "/langutil/static/css/main.295b288e.chunk.css"
+  },
+  {
+    "revision": "562ccf6a8b51fd375e3e",
+    "url": "/langutil/static/js/0.5201673c.chunk.js"
+  },
+  {
+    "revision": "daf6a155c5f739016492",
+    "url": "/langutil/static/js/10.11225911.chunk.js"
+  },
+  {
+    "revision": "9b383c223d03b851f9a1",
+    "url": "/langutil/static/js/3.0be6a239.chunk.js"
+  },
+  {
+    "revision": "23b77970e5383e054a44",
+    "url": "/langutil/static/js/4.a88ebba6.chunk.js"
+  },
+  {
+    "revision": "bd9a5785aef3532a220b",
+    "url": "/langutil/static/js/5.a2ca46d6.chunk.js"
+  },
+  {
+    "revision": "d9301644693032e88be0",
+    "url": "/langutil/static/js/6.02baca57.chunk.js"
+  },
+  {
+    "revision": "3c392da181d6657a031e",
+    "url": "/langutil/static/js/7.dd1364a7.chunk.js"
+  },
+  {
+    "revision": "890ef1870d9a8026bf9c",
+    "url": "/langutil/static/js/8.d0ebb8eb.chunk.js"
+  },
+  {
+    "revision": "27c29eaac6fa7ed9a9ea",
+    "url": "/langutil/static/js/9.96cbb979.chunk.js"
+  },
+  {
+    "revision": "cd37728678fa45e5ed03",
+    "url": "/langutil/static/js/main.772087cd.chunk.js"
+  },
+  {
+    "revision": "3c908b6125f82c7aaf7e",
+    "url": "/langutil/static/js/runtime-main.54bad45b.js"
+  },
+  {
+    "revision": "57b5d819f476cd370d6bddd826da6962",
+    "url": "/langutil/static/media/github-icon.57b5d819.svg"
+  },
+  {
+    "revision": "6370a5d28cf32545c7fd8b6e34c72288",
+    "url": "/langutil/static/media/langutil-icon.6370a5d2.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.446d7389e2675450b859982358db5389.js"
+  "/langutil/precache-manifest.97c6a9869116bb58d18a0f8b9d25ec85.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/langutil/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
