d253cb4cb1d4c39a92e640a4e7c9f3e851479984
chin98edwin <edwin_chin98@hotmail.com>
2019-11-02 20:18:13 +0800

    Home screen banner complete

--- docs-src/src/screens/HomeScreen/index.js
+++ docs-src/src/screens/HomeScreen/index.js
@@ -1,44 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { localize } from 'langutil'
 import { withLang } from 'langutil/react-additions'
-import { PATHS } from '../../constants'
 import CodeSamples from '../../code-samples'
-import { H1, Body } from '../../components/document'
+import { H1 } from '../../components/document'
+import HomeScreenBanner from '../../components/home-screen-banner'
 import './index.css'
 
 class HomeScreen extends React.Component {
-
+  state = { firstRenderComplete: false } // For performance boost
+  componentDidMount() { this.setState({ firstRenderComplete: true }) }
   render() {
+    const { firstRenderComplete } = this.state
     return (
       <div className='home-scn-container'>
-
-        <div className='home-scn-banner-container'>
-          <Body
-            className='home-scn-banner-title'
-            children={localize('LOC_JS_MADE_SIMPLE')}
-          />
-          <Link
-            className='home-scn-getStarted'
-            to={PATHS.docs}
-            children={localize({ keyword: 'GET_STARTED', casing: 'upperCase' })}
-          />
-        </div>
-
+        {firstRenderComplete ? <HomeScreenBanner /> : null}
         <div className='home-scn-howItWorks-container'>
           <div className=''>
             <H1
               style={{ textAlign: 'center' }}
               children={localize('HERES_HOW_IT_WORKS_NUTSHELL')}
             />
-            <CodeSamples.HomeDemo />
+            {firstRenderComplete ? <CodeSamples.HomeDemo /> : null}
           </div>
         </div>
-
       </div>
     )
   }
-
 }
 
 export default withLang(HomeScreen)
