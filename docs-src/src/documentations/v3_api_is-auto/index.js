047561dea7141420efb616a15a99253bb908a7dd
chin98edwin <edwin_chin98@hotmail.com>
2019-11-02 20:49:05 +0800

    Added doc content for Localizable

--- docs-src/src/documentations/v3_api_is-auto/index.js
+++ docs-src/src/documentations/v3_api_is-auto/index.js
@@ -4,11 +4,12 @@ import { withLang } from 'langutil/react-additions'
 import { Body } from '../../components/document'
 import CodeTitle from '../../components/code-title'
 import ReturnType from '../../components/return-type'
+import { STRINGS } from '../../constants'
 
 export default withLang(() => (
   <>
     <CodeTitle name='isAuto' rType='void' />
     <Body children={localize('API_DESC_IS_AUTO')} />
-    <ReturnType desc='-/-' type='boolean'/>
+    <ReturnType desc={STRINGS.notAvailableDash} type='boolean' />
   </>
 ))
