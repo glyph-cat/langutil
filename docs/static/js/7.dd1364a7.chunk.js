f344ca7fd65ac097dbe2840610da317003509b72
glyph-cat <b75ggnacj@relay.firefox.com>
2019-11-02 21:10:22 +0800

    Generated docs folder for the first time and ready to be served

---
+++ docs/static/js/7.dd1364a7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpdocs-src"]=this["webpackJsonpdocs-src"]||[]).push([[7],{164:function(c,t,a){"use strict";a.r(t);var s=a(0),e=a.n(s),n=a(17),o=a(4),r=a(1);t.default=function(){var c=Object(o.b)(),t=Object.values(c)[0][0].data[0].to;return e.a.createElement(n.a,{to:"".concat(r.PATHS.docs,"/").concat(t)})}}}]);
+//# sourceMappingURL=7.dd1364a7.chunk.js.map
