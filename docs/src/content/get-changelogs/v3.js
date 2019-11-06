4e362f7cb3e43f6eadd45a3678d28a0f0d504d5b
chin98edwin <edwin_chin98@hotmail.com>
2019-11-06 15:48:58 +0800

    Added 3.0.2 changelogs and localized v3 changelogs

--- docs/src/content/get-changelogs/v3.js
+++ docs/src/content/get-changelogs/v3.js
@@ -1,28 +1,66 @@
 import React from 'react'
+import { localize } from 'langutil'
 import { Code } from '../../components/document'
+import { withProps } from '../../modules'
 
 export default () => ({
   title: 'v3',
   data: [
     {
+      title: '3.0.2',
+      data: [
+        localize('CHANGELOG_ADDED_BACK_LEGACY_COMPENSATION'),
+        localize('CHANGELOG_WARN_FOR_PARAMOBJ_WILL_BE_SHOWN'),
+        localize({
+          keyword: 'CHANGELOG_FIX_INCR_TYPE_REACT_ADD',
+          transform: withProps({ add: <Code>react-additions</Code> })
+        }),
+        localize('CHANGELOG_CODE_OPTIMIZATIONS'),
+      ]
+    },
+    {
       title: '3.0.1',
       data: [
-        <>Fixed incorrect function name for <Code>AUTO_DETECT</Code> in Native Additions.</>,
-        <>Fixed issue where escaped placeholders do not level down if no param array or objects are provided (<Code>'%%p'</Code> → <Code>'%p'</Code> and <Code>'{'{::key}'}'</Code> → <Code>'{'{:key}'}'</Code>).</>,
-        'When applying casings, ALLCAPPED words and Names will be preserved.',
-        'Complete removal of deprecated APIs set to be in March 2020.',
-        'Minor code optimizations.',
+        localize({
+          keyword: 'CHANGELOG_FIXED_INCR_FN_NAME_AUTO_DETECT_NATIVE_ADD',
+          transform: withProps({ ad: <Code>AUTO_DETECT</Code> })
+        }),
+        localize({
+          keyword: 'CHANGELOG_FIXED_ESC_PHDRS',
+          transform: withProps({
+            p: <Code>'%p'</Code>, pp: <Code>'%%p'</Code>,
+            k: <Code>'{'{:key}'}'</Code>, kk: <Code>'{'{::key}'}'</Code>
+          })
+        }),
+        localize('CHANGELOG_WHEN_APPLY_CASING_ALLCAP_PRESERVED'),
+        localize('CHANGELOG_COMPLETE_REMOVAL_DEPRE_SET_MAR2020'),
+        localize('CHANGELOG_CODE_OPTIMIZATIONS'),
       ]
     },
     {
       title: '3.0.0',
       data: [
-        <>Localizing function is now simplified to only one function: <Code>localize()</Code></>,
-        <>New <Code>withLang()</Code> higher-order component in favor of <Code>{'<Localizable />'}</Code>.</>,
-        <>New <Code>AUTO_DETECT</Code> implementation.</>,
-        'Dictionary inspection now do not happen by default in favor of performance.',
-        'Structure your dictionary by keywords or language in the shape of an object, langutil will automatically determine which method you\'re using.',
-        <>New <Code>setDictionary()</Code> function in case you want to lazy load your localizations.</>,
+        localize({
+          keyword: 'CHANGELOG_LOC_FN_SIMPLIFIED',
+          transform: withProps({ loc: <Code>localize()</Code> })
+        }),
+        localize({
+          keyword: 'CHANGELOG_NEW_WITHLANG_HOC',
+          transform: withProps({
+            wl: <Code>withLang()</Code>,
+            locz: <Code>{'<Localizable />'}</Code>
+          })
+        }),
+        localize({
+          keyword: 'CHANGELOG_NEW_AUTO_DETECT_IMPLEMENTATION',
+          transform: withProps({ ad: <Code>AUTO_DETECT</Code> })
+        }),
+        localize('CHANGELOG_DICT_INSP_NOT_HAPPEN_DEFAULT'),
+        localize('CHANGELOG_STRUCT_DICT_AS_OBJ'),
+        localize({
+          keyword: 'CHANGELOG_NEW_SET_DICT_FN',
+          transform: withProps({ sd: <Code>setDictionary()</Code> })
+        }),
       ]
     }
   ]
