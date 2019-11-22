fba4114b74281925e87c16728f2425435805046e
glyph-cat <b75ggnacj@relay.firefox.com>
2019-11-22 17:14:25 +0800

    Localized v2 changelogs

--- docs/src/~content/get-changelogs/v2.js
+++ docs/src/~content/get-changelogs/v2.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { EXT_LINKS } from '~constants'
+import { localize } from 'langutil'
 import { Code } from '~components/document'
+import { EXT_LINKS } from '~constants'
+import { withProps } from '~modules'
 
 export default () => ({
   title: 'v2',
@@ -8,116 +10,168 @@ export default () => ({
     {
       title: '2.4.0',
       data: [
-        <><Code>{'<Localizable />'}</Code>  will update itself when <Code>setLanguage()</Code> is called.</>,
-        <>New <Code>allowEmpty</Code> parameter to suppress warnings about empty keywords.</>,
-        'Snooze dictionary inspection until a given date.',
+        localize({
+          keyword: 'CHANGELOG_LOCZ_WILL_UPDATE_ITSELF',
+          transform: withProps({
+            locz: <Code>{'<Localizable />'}</Code>,
+            setL: <Code>setLanguage()</Code>,
+          })
+        }),
+        localize({
+          keyword: 'CHANGELOG_NEW_ALLOW_EMPTY',
+          transform: withProps({
+            aEty: <Code>allowEmpty</Code>
+          })
+        }),
+        localize('CHANGELOG_SNOOZE_DICT_INSP_TILL_GIVEN_DATE'),
       ]
     },
     {
       title: '2.3.3',
       data: [
-        'Fixed issue where already-warned keywords still show up in group warnings.'
+        localize('CHANGELOG_FIXED_ISSUE_ALREADY_WARNED_STILL_SHOW_UP')
       ]
     },
     {
       title: '2.3.2',
       data: [
-        'Grouped warnings for missing localizations.',
-        <>You can pass in custom components via the <Code>renderAs</Code> prop in React Native like how it can be done in React.</>
+        localize('CHANGELOG_GROUPED_WARNINGS_FOR_MISSING_LOC'),
+        localize({
+          keyword: 'CHANGELOG_PASS_CUSTOM_COMP_VIA_RENDER_AS',
+          transform: withProps({ r: <Code>renderAs</Code> })
+        }),
       ]
     },
     {
       title: '2.3.1',
       data: [
-        'Hot fix for a problem in TypeScript.'
+        localize('CHANGELOG_HOTFIX_FOR_TS')
       ]
     },
     {
       title: '2.3.0',
       data: [
-        'Internal optimization for better performance and debugging experience.',
-        <>Automatic language detection for React Native is no longer included in the core to avoid conflict with webpack config in some cases. It has been splitted out as a function and can be imported from <Code>'langutil/native-additions'</Code>.</>,
-        <>The <a href={EXT_LINKS.dictionaryTemplateLink} children='repo' target='_blank' rel='noopener noreferrer' /> now includes template dictionaries, some of the most commonly used words are readily translated. More translations will be added over time.</>,
-        <>
-          Additions for React: <ul><li>New <Code>{'<Localizable />'}</Code> component that acts as a wrapper for HTML or React elements.</li></ul>
-        </>,
-        <>
-          Additions for React Native: <ul><li>New <Code>{'<Localizable />'}</Code> component that acts as a wrapper the <Code>{'<Text />'}</Code> tag.</li><li>Language detection for React Native has been splitted out from the core and moved into the additions folder.</li></ul>
-        </>,
+        localize('CHANGELOG_INTERNAL_OPTIMIZATIONS_PERF_DEBUG_XP'),
+        localize({
+          keyword: 'CHANGELOG_AUTO_DETECT_SPLITTED',
+          transform: withProps({ na: <Code>'langutil/native-additions'</Code> })
+        }),
+        localize({
+          keyword: 'CHANGELOG_THE_REPO_NOW_INCL_TEMPLATE_DICT',
+          transform: withProps({
+            repo: <a href={EXT_LINKS.dictionaryTemplateLink} children='repo' target='_blank' rel='noopener noreferrer' />
+          })
+        }),
+        localize({
+          keyword: 'CHANGELOG_ADDITIONS_FOR_REACT',
+          transform: withProps({ locz: <Code>{'<Localizable />'}</Code> })
+        })
       ]
     },
     {
       title: '2.2.4',
       data: [
-        'Added docs about the Dictionary.',
-        <>Changed <Code>'react-native'</Code> from optional dependency to peer dependency.</>
+        localize('CHANGELOG_ADDED_DOCS_ABT_DICT'),
+        localize({
+          keyword: 'CHANGELOG_CHG_RN_DEPENDENCY_TYPE',
+          transform: withProps({ rn: <Code>'react-native'</Code> })
+        })
       ]
     },
     {
       title: '2.2.3',
       data: [
-        'Fixed a bug with auto detect.'
+        localize('CHANGELOG_FIXED_BUG_AUTO_DETECT')
       ]
     },
     {
       title: '2.2.2',
       data: [
-        'Fixed the issue where false warnings about insufficient parameters are shown. For sure!'
+        localize('CHANGELOG_FIXED_FALSE_WARNINGS_PARAMS_FOR_SURE')
       ]
     },
     {
       title: '2.2.1',
       data: [
-        'Emergency fix for a bug where auto detect in v2.2.0 fails for React Native apps.'
+        localize('CHANGELOG_EMERGENCY_FIX_RN_AUTO_DETECT_FAIL')
       ]
     },
     {
       title: '2.2.0',
       data: [
-        <>In case the automatically detected language is not supported in the dictionary, langutil look for an alternative language from the dictionary. For example, if your dictionary contains localizations for <Code>'en'</Code>, but the automatically detected language is <Code>'en-us'</Code>, langutil will set the language to <Code>'en'</Code>.</>,
-        'Fixed an issue where false warnings about insufficient parameters are shown.',
-        <>New <Code>logs.focus()</Code> function allows to you see langutil logs in a specific block of code while hiding the rest, that is, only necessary if the logs were already hidden prior to that block.</>,
-        <>New <Code>isAuto()</Code> function to know whether autoDetection is set to true.</>
+        localize('CHANGELOG_WILL_LOOK_FOR_ALT_LANG_IF_AUTO_UNAVAILABLE'),
+        localize('CHANGELOG_FIXED_FALSE_WARNINGS_PARAMS'),
+        localize({
+          keyword: 'CHANGELOG_NEW_LOGS_FOCUS_FN',
+          transform: withProps({ f: <Code>logs.focus()</Code> })
+        }),
+        localize({
+          keyword: 'CHANGELOG_NEW_IS_AUTO_FN',
+          transform: withProps({ a: <Code>isAuto()</Code> })
+        }),
       ]
     },
     {
       title: '2.1.3',
       data: [
-        <>Silenced the unneccessary warning messages when <Code>casing</Code> and <Code>transform</Code> parameters in <Code>localizeWith()</Code> are not defined as they are optional.</>,
-        'Auto language detection is now supported in React Native.',
-        'Documentations have been updated to reduce package size.',
-        'Fixed the bug where langutil will always launch in minified mode by default.'
+        localize({
+          keyword: 'CHANGELOG_SILENCED_UNNECESSARY_WARN_IN_LOC_WITH',
+          transform: withProps({
+            cg: <Code>casing</Code>,
+            tr: <Code>transform</Code>,
+            lw: <Code>localizeWith()</Code>
+          })
+        }),
+        localize('CHANGELOG_AUTO_DETECT_SUPPORT_RN'),
+        localize('CHANGELOG_DOC_UPDATED_REDUCE_SIZE'),
+        localize('CHANGELOG_FIXED_ALWAYS_MINIFIED'),
       ]
     },
     {
       title: '2.1.2',
       data: [
-        'Fixed an issue where you may encounter an error with a message like "this.localize is not a function".'
+        localize('CHANGELOG_FIXED_ISSUE_THIS_LOCALIZE_NOT_FN')
       ]
     },
     {
       title: '2.1.1',
       data: [
-        'Fixed some documentation errors.',
-        <>Added new option <Code>'sentenceCase'</Code> for the <Code>casing</Code> parameter in <Code>localizeWith()</Code>.</>
+        localize('CHANGELOG_ADD_QUICKFIX_TO_DOC'),
+        localize({
+          keyword: 'CHANGELOG_ADDED_NEW_OPTION_LOC_WITH',
+          transform: withProps({
+            sc: <Code>'sentenceCase'</Code>,
+            cg: <Code>casing</Code>,
+            lw: <Code>localizeWith()</Code>
+          })
+        })
       ]
     },
     {
       title: '2.1.0',
       data: [
-        <>Added new method <Code>localizeWith()</Code> for more powerful localizing capabilities: Apply <b>casing styles</b> and <b>custom transformations</b> to the localized value! 🦄</>,
-        <>Added new method <Code>getDefinedLanguages()</Code> which allows you to access the list of languages defined in the dictionary during runtime.</>,
-        'You can now assign anything to your localized value, for instance, you might want to have a different logo image for each language if you have a tagline in your logo.',
-        'Fixed a critical bug where there production build fails if logs are shown.'
+        localize({
+          keyword: 'CHANGELOG_NEW_LOC_WITH_FN',
+          transform: withProps({ lw: <Code>localizeWith()</Code> })
+        }),
+        localize({
+          keyword: 'CHANGELOG_NEW_GET_DEF_LANGS',
+          transform: withProps({ gdl: <Code>getDefinedLanguages()</Code> })
+        }),
+        localize('CHANGELOG_ASSIGN_ANYTHING_TO_LOC_VALUE'),
+        localize('CHANGELOG_FIXED_PROD_BUILD_FAILS_IF_LOG_SHOWN'),
       ]
     },
     {
       title: '2.0.0',
       data: [
-        'You can now define dictionaries by Keywords.',
-        <>There is a new <Code>createKey()</Code> method to help you with that.</>,
-        'Language list now follows ISO language codes but the old convention that langutil 1.x.x still works.',
-        <><Code>index.d.ts</Code> integration.</>,
+        localize('CHANGELOG_CAN_DEFINE_DICT_BY_KEYWORDS'),
+        localize({
+          keyword: 'CHANGELOG_NEW_CREATEKEY_FN',
+          transform: withProps({ ck: <Code>createKey()</Code> })
+        }),
+        localize('CHANGELOG_LIST_FOLLOWS_ISO_CODES'),
+        localize('CHANGELOG_SUPPORT_TYPINGS'),
       ]
     }
   ]
