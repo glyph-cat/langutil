3cebe03dc23686907a590eaf181059351a36dd8b
chin98edwin <edwin_chin98@hotmail.com>
2019-12-23 11:46:25 +0800

    UI enhancements for compact screens

--- docs/src/~components/doc-prev-next/index.js
+++ docs/src/~components/doc-prev-next/index.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import { localize, getCurrentLanguage } from 'langutil'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { PATHS } from '~constants'
 import { scrollToTop } from '~modules'
 import SectionContext from '~contexts/SectionContext'
 import './index.css'
 
+function MediaQuery({ onChange }) {
+  const isCompact = useMediaQuery('(max-height: 600px), (max-width: 800px)')
+  useEffect(() => {
+    onChange(isCompact)
+  }, [onChange, isCompact])
+  return null
+}
+
 class DocPrevNext extends React.Component {
 
   static contextType = SectionContext
 
+  state = { isCompact: false }
   lang = getCurrentLanguage()
   sectionIndices = []
   flatSections = null
@@ -28,66 +38,73 @@ class DocPrevNext extends React.Component {
       this.flatSections = flattened
       this.sectionIndices = indices
     }
-    // console.log('this.flatSections:', this.flatSections)
     return this.flatSections
   }
 
+  handleWidthChange = (isCompact) => {
+    this.setState({ isCompact })
+  }
+
   render() {
-    const { match: { params: { version, section, id } } } = this.props
+    const { position, match: { params: { version, section, id } } } = this.props
     this.prepareFlattenedSections()
-    const getIndexOfIdInIndices = (version, section, id, indices) => {
-      // console.log({ version, section, id, indices })
-      for (let i = 0; i < indices.length; i++) {
-        // console.log(`${version}/${section}/${id}`, indices[i])
-        if (`${version}/${section}/${id}` === indices[i]) { return i }
-      }
-      return -1
-    }
     const indexOfInSections = getIndexOfIdInIndices(version, section, id, this.sectionIndices)
-    // console.log('indexOfInSections:', indexOfInSections)
+    const { isCompact } = this.state
 
     let prev, next
     if (this.flatSections[indexOfInSections - 1]) {
-      prev = this.flatSections[indexOfInSections - 1]
+      if (!(position === 'bottom' && isCompact)) {
+        prev = this.flatSections[indexOfInSections - 1]
+      }
     }
     if (this.flatSections[indexOfInSections + 1]) {
-      next = this.flatSections[indexOfInSections + 1]
+      if (!(position === 'top' && isCompact)) {
+        next = this.flatSections[indexOfInSections + 1]
+      }
     }
-    // console.log('prevItem:', prev)
-    // console.log('nextItem:', next)
 
     return (
-      <div className='doc-prev-next-container'>
-        {prev ?
-          <Link
-            className='doc-prev-next-link'
-            to={`${PATHS.docs}/${prev.to}`}
-            onClick={scrollToTop}
-            children={localize('DOC_PREV_PARAM', [smartTrim(prev.text)])}
-            title={prev.text}
-          />
-          :
+      <>
+        <MediaQuery onChange={this.handleWidthChange} />
+        <div className='doc-prev-next-container'>
+          {prev ?
+            <Link
+              className='doc-prev-next-link'
+              to={`${PATHS.docs}/${prev.to}`}
+              onClick={scrollToTop}
+              children={localize('DOC_PREV_PARAM', [smartTrim(prev.text)])}
+              title={prev.text}
+            />
+            :
+            <div />
+          }
           <div />
-        }
-        <div />
-        {next ?
-          <Link
-            className='doc-prev-next-link'
-            to={`${PATHS.docs}/${next.to}`}
-            onClick={scrollToTop}
-            children={localize('DOC_NEXT_PARAM', [smartTrim(next.text)])}
-            title={next.text}
-          />
-          :
-          <div />
-        }
-      </div>
+          {next ?
+            <Link
+              className='doc-prev-next-link'
+              to={`${PATHS.docs}/${next.to}`}
+              onClick={scrollToTop}
+              children={localize('DOC_NEXT_PARAM', [smartTrim(next.text)])}
+              title={next.text}
+            />
+            :
+            <div />
+          }
+        </div>
+      </>
     )
   }
 
 }
 
 export default withRouter(DocPrevNext)
+
+function getIndexOfIdInIndices(version, section, id, indices) {
+  for (let i = 0; i < indices.length; i++) {
+    if (`${version}/${section}/${id}` === indices[i]) { return i }
+  }
+  return -1
+}
 
 /**
  * @description Trims a string by word if it's too long.
