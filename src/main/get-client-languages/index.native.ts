5cfe0447edc58200a1789f261d65daf8367e96ae
chin98edwin <edwin_chin98@hotmail.com>
2021-07-26 17:57:13 +0800

    Added types to hook-interface.tsx

--- src/main/get-client-languages/index.native.ts
+++ src/main/get-client-languages/index.native.ts
@@ -2,11 +2,6 @@
 import { NativeModules, Platform } from 'react-native'
 import { LangutilLanguage } from '../../schema'
 
-/**
- * Gets the available languages on the current device.
- * @public
- * @returns A a string representing the client language or null if unavailable.
- */
 function getClientLanguages(): Array<LangutilLanguage> | null {
   const getLocale = Platform.select({
     android: () => {
