13c43033a91310335d45cbe94cf6726d44c53f43
chin98edwin <edwin_chin98@hotmail.com>
2021-07-28 18:35:44 +0800

    Fixed incorrect node resolved extensions

---
+++ src/main/get-client-languages/index.main.ts
@@ -0,0 +1,8 @@
+// import getClientLanguagesMain from './index.main'
+// import getClientLanguagesNative from './index.native'
+
+// const getClientLanguages = process.env.IS_RN_BUILD === 'true'
+//   ? getClientLanguagesNative
+//   : getClientLanguagesMain
+
+// export default getClientLanguages
